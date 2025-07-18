bb588c743a6f9d37c74ad0f3f0fee87e23de2104
Johannes Fink <j.fink@fruva.de>
2025-07-18 10:45:02 +0200

    feat: Add API DTO models

---
+++ FruVa.Ordering.ApiAccess/Models/Article.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FruVa.Ordering.ApiAccess.Models
+{
+    public class Article
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("mainArticleNumber")]
+        public int MainArticleNumber { get; set; }
+
+        [JsonPropertyName("detailArticleNumber")]
+        public int DetailArticleNumber { get; set; }
+
+        [JsonPropertyName("articleName")]
+        public string ArticleName { get; set; }
+
+        [JsonPropertyName("packageSize")]
+        public string PackageSize { get; set; }
+
+        [JsonPropertyName("articleGroupNumber")]
+        public string ArticleGroupNumber { get; set; }
+
+        [JsonPropertyName("articleGroupName")]
+        public string ArticleGroupName { get; set; }
+
+        [JsonPropertyName("originCountry")]
+        public string OriginCountry { get; set; }
+
+        [JsonPropertyName("tradeClass")]
+        public string TradeClass { get; set; }
+
+        [JsonPropertyName("colli")]
+        public float Colli { get; set; }
+
+        [JsonPropertyName("caliber")]
+        public string Caliber { get; set; }
+
+        [JsonPropertyName("variety")]
+        public string Variety { get; set; }
+
+        [JsonPropertyName("ownBrand")]
+        public string OwnBrand { get; set; }
+
+        [JsonPropertyName("searchQuery")]
+        public string SearchQuery { get; set; }
+
+        [JsonPropertyName("rowVersion")]
+        public string RowVersion { get; set; }
+    }
+
+}
---
+++ FruVa.Ordering.ApiAccess/Models/Recipient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FruVa.Ordering.ApiAccess.Models
+{
+    public class Recipient
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("street")]
+        public string Street { get; set; }
+
+        [JsonPropertyName("streetNumber")]
+        public string StreetNumber { get; set; }
+
+        [JsonPropertyName("postCode")]
+        public string PostCode { get; set; }
+
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+
+        [JsonPropertyName("rowVersion")]
+        public string RowVersion { get; set; }
+    }
+}
