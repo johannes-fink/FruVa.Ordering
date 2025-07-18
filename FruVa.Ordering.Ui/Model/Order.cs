c61ca594ad7f671388bff031bafc2018c56615a6
Johannes Fink <j.fink@fruva.de>
2025-07-18 08:05:23 +0200

    feat: Add Order UI model

---
+++ FruVa.Ordering.Ui/Model/Order.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FruVa.Ordering.Ui.ViewModel;
+using System.Collections.ObjectModel;
+
+namespace FruVa.Ordering.Ui.Model
+{
+    public partial class Order : ObservableObject
+    {
+        public decimal? TotalPrice => OrderDetails.Sum(x => x.Price);
+
+        public int OrderNumber { get; set; }
+        public required string RecipientName { get; set; }
+        
+        [ObservableProperty]
+        public ObservableCollection<OrderDetail> _orderDetails = [];
+    }
+}
