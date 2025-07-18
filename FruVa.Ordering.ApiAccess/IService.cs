fe9b19611d07e76d851bcd34c358f342cfbc30ae
Johannes Fink <j.fink@fruva.de>
2025-07-18 10:46:19 +0200

    feat: Implement API service to get recipients and articles

---
+++ FruVa.Ordering.ApiAccess/IService.cs
@@ -0,0 +1,17 @@
+﻿using FruVa.Ordering.ApiAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruVa.Ordering.ApiAccess
+{
+    public interface IService
+    {
+        Task<List<Article>> GetArticlesAsync();
+        Task<List<Recipient>> GetRecipientsAsync();
+        Task<Article?> GetArticleByIdAsync(Guid id);
+        Task<Recipient?> GetRecipientByIdAsync(Guid id);
+    }
+}
---
+++ FruVa.Ordering.ApiAccess/Service.cs
@@ -0,0 +1,79 @@
+﻿using FruVa.Ordering.ApiAccess.Models;
+using System.Text.Json;
+
+namespace FruVa.Ordering.ApiAccess
+{
+    public class Service : IService
+    {
+        private readonly Uri BASE_URL = new Uri("https://localhost:61404/api/");
+        private HttpClient HttpClient {  get; set; }
+
+        public Service()
+        {
+            HttpClient = new HttpClient() { BaseAddress = BASE_URL };
+        }
+
+        public async Task<List<Article>> GetArticlesAsync()
+        {
+            var response = await HttpClient.GetAsync("articles");
+
+            if (response.IsSuccessStatusCode == false)
+            {
+                // TODO: Error => Log and inform user
+                return [];
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var articles = JsonSerializer.Deserialize<List<Article>>(json);
+
+            return articles ?? [];
+        }
+
+        public async Task<List<Recipient>> GetRecipientsAsync()
+        {
+            var response = await HttpClient.GetAsync("recipients");
+
+            if (response.IsSuccessStatusCode == false)
+            {
+                // TODO: Error => Log and inform user
+                return [];
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var recipients = JsonSerializer.Deserialize<List<Recipient>>(json);
+
+            return recipients ?? [];
+        }
+
+        public async Task<Article?> GetArticleByIdAsync(Guid id)
+        {
+            var response = await HttpClient.GetAsync($"articles/{id}");
+
+            if (response.IsSuccessStatusCode == false)
+            {
+                // TODO: Error => Log and inform user
+                return null;
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var article = JsonSerializer.Deserialize<Article>(json);
+
+            return article;
+        }
+        public async Task<Recipient?> GetRecipientByIdAsync(Guid id)
+        {
+            var response = await HttpClient.GetAsync($"Recipients/{id}");
+
+            if (response.IsSuccessStatusCode == false)
+            {
+                // TODO: Error => Log and inform user
+                return null;
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var recipient = JsonSerializer.Deserialize<Recipient>(json);
+
+            return recipient;
+        }
+    }
+}
