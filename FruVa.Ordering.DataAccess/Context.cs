ffe65645f2351fea60c28ec9063d9edcd2936a3a
Johannes Fink <j.fink@fruva.de>
2025-07-21 16:00:34 +0200

    Contex

---
+++ FruVa.Ordering.DataAccess/Context.cs
@@ -0,0 +1,17 @@
+﻿using FruVa.Ordering.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FruVa.Ordering.DataAccess
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=Orders;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
