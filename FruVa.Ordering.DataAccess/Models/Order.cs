af3aaaf72cc74812a254bc49c5995d36437eb446
Johannes Fink <j.fink@fruva.de>
2025-07-21 16:00:27 +0200

    Property Orders

---
+++ FruVa.Ordering.DataAccess/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FruVa.Ordering.DataAccess.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int OrderNumber { get; set; }
+
+        [Required]
+        public Guid RecipientId { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; } = [];
+    }
+}
---
+++ FruVa.Ordering.DataAccess/Models/OrderDetail.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FruVa.Ordering.DataAccess.Models
+{
+    [Table("OrderDetails")]
+    public class OrderDetail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public Guid ArticleId { get; set; }
+
+        [Required]
+        public Order Order { get; set; }
+    }
+}
