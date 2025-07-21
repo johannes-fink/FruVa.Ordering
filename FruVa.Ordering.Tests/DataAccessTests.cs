d376af3492e57de0daa8d3edd1cc71226e85f295
Johannes Fink <j.fink@fruva.de>
2025-07-21 16:01:01 +0200

    Test for Ordering

---
+++ FruVa.Ordering.Tests/DataAccessTests.cs
@@ -0,0 +1,46 @@
+﻿using FruVa.Ordering.DataAccess.Models;
+
+namespace FruVa.Ordering.Tests
+{
+    [TestClass]
+    public class DataAccessTests
+    {
+        private static DataAccess.Context? _db;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _db = new DataAccess.Context();
+        }
+
+        [TestMethod]
+        public void CanSaveOrder()
+        {
+            var order = new Order
+            {
+                OrderNumber = 20,
+                RecipientId = Guid.NewGuid(),
+                OrderDetails = new List<OrderDetail>
+                {
+                    new()
+                    {
+                        ArticleId = Guid.NewGuid(),
+                        Price = 100,
+                        Quantity = 1,
+                    }
+                }
+            };
+
+            try
+            {
+                _db!.Orders.Add(order);
+                _db.SaveChanges();
+            }
+            finally
+            {
+                _db!.Orders.Remove(order);
+                _db.SaveChanges();
+            }
+        }
+    }
+}
