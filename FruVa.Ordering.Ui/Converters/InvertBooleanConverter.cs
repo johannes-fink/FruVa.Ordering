01c1a7f35b68788bbd9765e0e0001072de6d80ea
Johannes Fink <j.fink@fruva.de>
2025-07-23 10:29:34 +0200

    Connverter

---
+++ FruVa.Ordering.Ui/Converters/InvertBooleanConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace FruVa.Ordering.Ui.Converters
+{
+    public class InvertBooleanConverter : IValueConverter
+    {
+        public InvertBooleanConverter()
+        {
+            
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not bool booleanValue)
+            {
+                return false;
+            }
+
+            return ! booleanValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not bool booleanValue)
+            {
+                return false;
+            }
+
+            return ! booleanValue;
+        }
+    }
+}
